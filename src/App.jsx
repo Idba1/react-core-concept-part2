461452e06c6ff08c7378999cb9fbb7fda40c0960
Idba1 <moniraislamself@gmail.com>
2024-03-13 14:30:51 +0600

    arrow function

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import './App.css'
 
 function App() {
 
-  function clickMe(){
+  function clickMe() {
     alert('hello from ClickMe function');
   }
 
+  const clickMe2 = () => {
+    alert('hello from Arrow function');
+  }
   return (
     <>
       <h1>React Core concept part 2</h1>
       <button onClick={clickMe}>Click me</button>
+      <button onClick={clickMe2}>Click me2</button>
     </>
   )
 }
