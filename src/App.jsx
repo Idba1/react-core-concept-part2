581a9b67c2ed5003f773a0a915c9777fcf19445c
Idba1 <moniraislamself@gmail.com>
2024-03-13 14:37:17 +0600

    parameters in function

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,18 @@ function App() {
   const clickMe2 = () => {
     alert('hello from Arrow function');
   }
+
+
+  const clickMe4 = (num) => {
+    alert(num + 6);
+  }
   return (
     <>
       <h1>React Core concept part 2</h1>
       <button onClick={clickMe}>Click me</button>
       <button onClick={clickMe2}>Click me2</button>
       <button onClick={() => {alert('click from arrow function in onClick')}}>Click me3</button>
+      <button onClick={() => clickMe4(5)}>Click me4</button>
     </>
   )
 }
